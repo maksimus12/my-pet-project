157f0e54121fe70707b8e3b5d5e8677f6ea6ce85
maksimus12 <maks.diacenko@mail.ru>
2024-08-06 15:22:25 +0300

    Menu links bug not fixed

--- src/Views/templates/menu.php
+++ src/Views/templates/menu.php
@@ -1,7 +1,7 @@
 <div class="navbar bg-dark d-flex justify-content-center">
     <div class="d-flex justify-content-center ">
-      <a type="button" class="btn btn-dark m-2" href="/">Home</a>
-        <a type="button" class="btn btn-dark m-2" href="/users">Users</a>
-        <a type="button" class="btn btn-dark m-2" href="/users/add">Add user</a>
+      <a type="button" class="btn btn-dark m-2" href="/my-pet-project/public/">Home</a>
+        <a type="button" class="btn btn-dark m-2" href="/my-pet-project/public/users">Users</a>
+        <a type="button" class="btn btn-dark m-2" href="/my-pet-project/public/users/add">Add user</a>
     </div>
 </div>
